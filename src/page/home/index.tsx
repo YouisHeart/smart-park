a9ba4d98d884e3e28a83b7cd276d4fb426332e1c
高晨建 <2422892983@qq.com>
2026-01-23 10:18:14 +0800

    提交前端代码

---
+++ src/page/home/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from 'antd'
+function Home() {
+    return <div>
+        我是Home组件
+        <Button type="primary">Primary Button</Button>
+    </div>
+}
+
+export default Home
---
+++ src/page/login/index.tsx
@@ -0,0 +1,5 @@
+function Login() {
+    return <div>我是Login组件</div>
+}
+
+export default Login
